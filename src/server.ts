6fc37ae191428a21eac19d101539d278a0bd9da4
illi0125 <illikamalov10@gmail.com>
2026-05-22 00:30:46 +0900

    feat: integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import moment from 'moment';   // const moment = require('moment');
+import dotenv from "dotenv";
+dotenv.config();
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
