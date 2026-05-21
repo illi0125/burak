6915a426d3d84ca9217856b2c6db41c33f1f7df0
illi0125 <illikamalov10@gmail.com>
2026-05-22 00:41:09 +0900

    feat: create train area

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,22 @@
+console.log("Hello world!");
+
 // MITASK L-TASK
-function reverseSentence(str) {
+function reverseSentence(str: string): string {
   let words = str.split(" ");
-  let result = " ";
+  let result = "";
+
   for (let i = 0; i < words.length; i++) {
-    let reversed = " ";
+    let reversed = "";
+
     for (let j = words[i].length - 1; j >= 0; j--) {
       reversed += words[i][j];
     }
 
     result += reversed + " ";
   }
+
   return result.trim();
 }
+
 console.log(reverseSentence("we like coding"));
 console.log(reverseSentence("leo"));
