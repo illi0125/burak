7e37f6948627490d820baa68ef124ea8b680f5ae
illi0125 <illikamalov10@gmail.com>
2026-06-06 20:57:42 +0900

    feat: create product controller and product routing apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 /* Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -12,6 +13,12 @@ routerAdmin
   .post("/signup", restaurantController.processSignup);
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
+
 /* Product */
+routerAdmin.get("/product/all", productController.getAllProduct);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
+
 /* User */
+
 export default routerAdmin;
