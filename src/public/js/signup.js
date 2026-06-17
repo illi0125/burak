1464be861b5bff5f1e53cd316102f5934ba5d2db
illi0125 <illikamalov10@gmail.com>
2026-06-17 16:04:37 +0900

    feat: finished frontend signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,8 +1,27 @@
 console.log("Signup frontend javascript file");
 
 $(function () {
-  $(".member-nick").click(function () {
-    $(".member-phone").toggle();
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
   });
 });
 
@@ -24,6 +43,14 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert("Password differs, please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
     return false;
   }
 }
