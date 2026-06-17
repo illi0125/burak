b21e0c796ebee4cf2c5975b301ce3101d43e5e1e
illi0125 <illikamalov10@gmail.com>
2026-06-17 14:15:12 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,29 @@
 console.log("Signup frontend javascript file");
+
+$(function () {
+  $(".member-nick").click(function () {
+    $(".member-phone").toggle();
+  });
+});
+
+function validateSignupForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    memberPassword === ""
+  ) {
+    alert("Please insert all required inputs!");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert("Password differs, please check!");
+    return false;
+  }
+}
