055927cfbba6f0c08ed1909e8eeed40d29aaf321
illi0125 <illikamalov10@gmail.com>
2026-07-04 00:25:50 +0900

    feat: develop authentication via tokens with cookies

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,14 @@ import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from "./libs/types/common";
-
 // REACT
-import cors from 'cors';
+import cors from "cors";
 
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
@@ -23,11 +23,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
 // REACT
 // Allow your upcoming Vite frontend to access this backend
-app.use(cors({ origin: 'http://localhost:5173' })); 
+app.use(cors({ origin: "http://localhost:5173" }));
 
 /** 2-SESSIONS **/
 app.use(
@@ -52,7 +53,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/admin", routerAdmin); // SSR: EJS 
+app.use("/admin", routerAdmin); // SSR: EJS
 app.use("/", router); // SPA: REACT
 
 export default app;
