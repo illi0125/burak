7d1d35027afd9a2ce65ede67f3e5a6f4991ecaa0
illi0125 <illikamalov10@gmail.com>
2026-07-10 11:43:55 +0900

    feat: develop updateOrder restApi

--- src/router.ts
+++ src/router.ts
@@ -42,5 +42,10 @@ router.get(
   memberController.verifyAuth,
   orderController.getMyOrders,
 );
+router.post(
+  "/order/update",
+  memberController.verifyAuth,
+  orderController.updateOrder,
+);
 
 export default router;
